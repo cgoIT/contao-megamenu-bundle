e4d815370dc7e646eb90a9a5bd73dab119ca0259
cgoIT <info@cgo-it.de>
2025-02-11 08:24:00 +0100

    chore: fix copyright

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of cgoit\contao-megamenu-bundle for Contao Open Source CMS.
  *
- * @copyright  Copyright (c) 2024, cgoIT
+ * @copyright  Copyright (c) 2025, cgoIT
  * @author     cgoIT <https://cgo-it.de>
  * @license    LGPL-3.0-or-later
  */
